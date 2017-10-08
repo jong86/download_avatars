d53bbc46a21a4705ca4f7b3d31ce36e08e5e15b5
Jon Gaspar <jon_gaspar@hotmail.com>
2017-10-08 20:38:38 +0000

    Added request that returns all contributors to specified repo

--- download_avatars.js
+++ download_avatars.js
@@ -3,4 +3,24 @@ const repo = process.argv[3];
 
 const log = console.log;
 
-log(repoOwner, repo);
+const request = require("request");
+
+var options = {
+  url: `https://api.github.com/repos/${repoOwner}/${repo}/contributors`,
+  headers: {
+    "User-Agent": "jong86"
+  }
+};
+
+function callbackContributors(error, response, body) {
+  if (!error && response.statusCode == 200) {
+    let info = JSON.parse(body);
+    for (let i = 0; i < info.length; i++) {
+      console.log(info[i].login);
+    }
+  }
+}
+
+request(options, callbackContributors);
+
+//options.url = `URL_FOR_EACH_CONTRIBUTOR`;
