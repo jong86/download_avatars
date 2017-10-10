d55b93165448901799d5e7a34b1e453e52326296
Jon Gaspar <jon_gaspar@hotmail.com>
2017-10-10 00:21:00 +0000

    Prints contributor names and image urls

--- download_avatars.js
+++ download_avatars.js
@@ -4,24 +4,40 @@ var repoName = process.argv[3];
 var log = console.log;
 
 var request = require("request");
+var https = require('https');
 var fs = require('fs');
 
 
 var GITHUB_USER = "jong86";
 var GITHUB_TOKEN = "e508af8416da135ae64457edbe016e70b0c11e09";
 
-var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+
+
 
 function getRepoContributors(repoOwner, repoName, cb) {
-
   
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  var options = {
+    url: requestURL,
+    headers: {
+      "User-Agent": "jong86"
+    }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
 }
 
-console.log(requestURL);
 
 getRepoContributors(repoOwner, repoName, function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
+  console.log("Errors:", err, '\n');
+  let data = JSON.parse(result);
+  for (let i = 0; i < data.length; i++) {
+    console.log(data[i].login + ": " + data[i].avatar_url)
+    
+  }
+
 });
 
 
